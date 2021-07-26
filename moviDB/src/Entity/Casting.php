b9b9c10898190a3396abefccaf35de4434b85604
quentin-j <quentin678jougla@gmail.com>
2021-07-26 14:39:33 +0200

    ajout de entité Casting

---
+++ moviDB/src/Entity/Casting.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CastingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CastingRepository::class)
+ */
+class Casting
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $role;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $creditOrder;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getCreditOrder(): ?int
+    {
+        return $this->creditOrder;
+    }
+
+    public function setCreditOrder(int $creditOrder): self
+    {
+        $this->creditOrder = $creditOrder;
+
+        return $this;
+    }
+}
