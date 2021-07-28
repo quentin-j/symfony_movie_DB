4bcf1dc78dd14bec94d194d365dfe09c5251c8ae
quentin-j <quentin678jougla@gmail.com>
2021-07-28 14:45:03 +0200

    ajout de données à la main plus gestion de l'unicité

---
+++ moviDB/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Casting;
+use App\Entity\Genre;
+use App\Entity\Movie;
+use App\Entity\Person;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $entityManager)
+    {
+        // Création de genres
+        // On va garder une trace des entités générées pour pouvoir les réutiliser dans les relations
+        $genres = [];
+        for ($i = 0 ; $i < 10 ; $i++)
+        {
+            $genre= new Genre();
+            $genre->setName('Genre ' . $i);
+            $entityManager->persist($genre);
+
+            // On veut ce souvenir de tous les genres
+            $genres[] = $genre;
+        }
+
+        //création de movie
+        $movies = [];
+        for ($i = 0 ; $i < 50 ; $i++)
+        {
+            $movie = new Movie();
+            $movie->setTitle('movie ' . $i);
+
+            // on définit par film un nombre de genre à ajouter
+            $nbGenreToAdd = 8;
+            $keysToAdd = array_rand($genres, $nbGenreToAdd);
+            // array_rand renvoi soit une clef soit un tableau
+            // Dans le cas ou on reçoit une clef, on en fait un tableau pour que la boucle fonctionne
+            if (! is_array($keysToAdd))
+            {
+                $keysToAdd = [$keysToAdd];
+            }
+            foreach ($keysToAdd as $key) 
+            {
+                $movie->addGenre($genres[$key]);
+            }
+            $entityManager->persist($movie);
+            $movies[] = $movie;
+        }
+
+        // Création des personnes
+        $persons = [];
+        for ($i = 0 ; $i < 500 ; $i++) 
+        {            
+            $person = new Person();
+            $person->setName('person '. $i);
+            $entityManager->persist($person);
+
+            $persons[] = $person;
+        }
+
+        // Création des castings
+        for ($i = 0 ; $i < 500 ; $i++) 
+        {
+            $casting = new Casting();
+
+            // On récupère une personne et un movie depuis nos listes
+            $castingMovie = $movies[rand(0, count($movies) - 1)];
+            $castingPerson = $persons[rand(0, count($persons) - 1)];
+            $casting
+            ->setMovie($castingMovie)
+            ->setPerson($castingPerson)
+            ->setRole('ROLE ' . $i)
+            ->setCreditOrder(rand(0, $i));
+            $entityManager->persist($casting);
+        }
+
+        $entityManager->flush();
+    }
+}
+// composer require fakerphp/faker
