ad71851aceeb8552002573ba0fbc536cd35f5375
quentin-j <quentin678jougla@gmail.com>
2021-08-18 14:57:39 +0200

    CRUD : User + correction d'affichage + hashage du mot de passe

---
+++ moviDB/src/Form/UserType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'constraints' => [
+                    new Email()
+                ]
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label' => 'rôle',
+                'multiple' =>true,
+                'expanded' =>true,
+                'choices' => [
+                    'Administrateur' => "ROLE_ADMIN",
+                    'Utilisateur' => "ROLE_USER",
+                ]
+            ])
+            ->add('password')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
